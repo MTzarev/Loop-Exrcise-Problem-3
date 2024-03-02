e63a1bb4907451ae1a49a3990d2053d466c6b841
Miroslav Tzarev <mtzarev@gmail.com>
2024-03-02 23:18:52 +0200

    Add files via upload

---
+++ 03. Excellent grade.js
@@ -0,0 +1,10 @@
+function excellentGrade (grade){
+    grade = Number(grade)
+if (grade >=5.50){
+    console.log ("Excellent");
+}else {
+    console.log ("Not excellent")
+}
+    console.log()
+}
+excellentGrade(5.40)
